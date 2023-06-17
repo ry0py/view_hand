36bc8f48123fe7aff74399a2fde55982d3b981b4
ryoppy101@gmail.com <ryoppy101@gamil.com>
2023-06-17 18:37:50 +0900

    flaskで遊んでみる

--- src/detect.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-#model = YOLO("yolov8n.yaml")  # build a new model from scratch
-model = YOLO("./runs/detect/train3/weights/best.pt")  # load a .pt file from disk
-# Use the model
-results = model("./data/test/708544955.697029.mp4",save=True)  # predict on an image フォルダ形式じゃないといけない&\には対応してないからエラー出るから気を付けて
--- src/train.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-# Train the model
-model = YOLO('yolov8n.pt')
-model.train(data='./data/hand.yaml', epochs=150)
