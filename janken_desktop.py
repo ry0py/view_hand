5f9377eb5e549eb4013a0d5b249db3b9d330f04b
ryoppy101@gmail.com <ryoppy101@gamil.com>
2023-06-23 01:36:39 +0900

    できちゃ

--- janken_desktop.py
+++ janken_desktop.py
@@ -11,19 +11,36 @@ class win(tk.Frame):
         self.master.resizable(False, False) # リサイズ設定
         label_1a = tk.Label( text='じゃんけんゲーム', font=('', 30))
         label_1a.pack()
-        button_lose = tk.Button( text='負けた', command=self.getwin(win= "lose"))
+        button_lose = tk.Button( text='負けた', command=self.getwin_lose)#getwinに引数与えてやるとなぜか実行される
         button_lose.pack(padx=5, pady=10)
-        button_win = tk.Button( text='勝った', command=self.getwin(win= "win"))
+        button_win = tk.Button( text='勝った', command=self.getwin_win)
         button_win.pack(padx=5, pady=10)
-        button_draw = tk.Button( text='あいこ', command=self.getwin(win= "draw"))
-        button.pack(padx=5, pady=10)
+        button_draw = tk.Button( text='あいこ', command=self.getwin_draw)
+        button_draw.pack(padx=5, pady=10)
 
-    def getwin(self,win = "win"):
+    def getwin_lose(self):
         subWindow = tk.Toplevel(self.master)
         subWindow.title('勝敗') # 画面タイトル設定
         subWindow.geometry('500x500')  # 画面サイズ設定
         subWindow.resizable(False, False) # リサイズ設定
-        label_sub = tk.Label( subWindow,text=dh.DecideAIHand(battle = win), font=('', 30))
+        label_sub = tk.Label( subWindow,text=dh.DecideAIHand(battle="lose"), font=('', 30))
+        label_sub.pack()
+    def getwin_win(self):
+        subWindow = tk.Toplevel(self.master)
+        subWindow.title('勝敗') # 画面タイトル設定
+        subWindow.geometry('500x500')  # 画面サイズ設定
+        subWindow.resizable(False, False) # リサイズ設定
+        label_sub = tk.Label( subWindow,text=dh.DecideAIHand(battle="win"), font=('', 30))
+        label_sub.pack()
+        image = PhotoImage() 
+        label_3 = Label(frame3, image=image)
+        label_3.pack(pady=20)   
+    def getwin_draw(self):
+        subWindow = tk.Toplevel(self.master)
+        subWindow.title('勝敗') # 画面タイトル設定
+        subWindow.geometry('500x500')  # 画面サイズ設定
+        subWindow.resizable(False, False) # リサイズ設定
+        label_sub = tk.Label( subWindow,text=dh.DecideAIHand(battle="draw"), font=('', 30))
         label_sub.pack()
 
 if __name__ == '__main__':
