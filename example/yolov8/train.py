8bd402f57a06008e18c88c7d16c5175f5021d03c
ryoppy101@gmail.com <ryoppy101@gamil.com>
2023-10-26 08:19:35 +0900

    train4取得

--- example/yolov8/train.py
+++ example/yolov8/train.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 # Train the model
-model = YOLO('yolov8n.pt')
-model.train(data='./data/hand.yaml', epochs=64)
+model = YOLO('./runs/detect/train2/weights/best.pt')
+model.train(data='./data/hand.yaml', epochs=100)
